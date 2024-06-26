72c26ee04761d7ad2bb007b62c6dce4e36b5a666
Tereza Beránková <xteryna@seznam.cz>
2024-06-26 19:12:38 +0200

    rozdělení stránky na hlavní komponenty: Header, Banner, Menu, GAllery, Contact, Footer

---
+++ src/components/footer/index.jsx
@@ -0,0 +1,11 @@
+import './index.css'
+
+export const Footer = () => {
+    return (
+        <div className="container">
+            <div className="footer__content">
+                Café Lóra je tréningový projekt v rámci Czechitas kurzu JavaScript 2
+            </div>
+        </div>
+    )
+}
